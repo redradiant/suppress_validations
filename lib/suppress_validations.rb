0c069b19058473fd83ddf3b4761e1ccacbd18d2f
Matt Lightner <mlightner@gmail.com>
2010-11-10 06:40:36 -0800

    Rails 3 support.

--- lib/suppress_validations.rb
+++ lib/suppress_validations.rb
@@ -1,42 +1,48 @@
+require 'active_support/concern'
+
 module SuppressValidations
-  def self.included(klass)
-    ActiveRecord::Base.class_eval do
-      extend ClassMethods
-      alias_method :original_valid?, :valid?
+  extend ActiveSuppoer::Concern
 
-      def valid?
-        if self.class.validations_disabled?
-          true
-        else
-          original_valid?
-        end
-      end
-    end
-
-    klass.send :include, InstanceMethods
-  end
-  
-  module ClassMethods
-    def disable_validations!
-      @@validations_disabled = true
-    end
-    
-    def enable_validations!
-      @@validations_disabled = false
-    end
-
-    def validations_disabled?
-      @@validations_disabled ||= false
-    end
+  included do
+    attr_accessor :validations_disabled
+    after_initialize :"enable_validations!"
   end
 
   module InstanceMethods
-    def suppress_validations
-      ActiveRecord::Base.disable_validations!
-      ret = yield
-      ActiveRecord::Base.enable_validations!
 
-      return ret
+    def valid?
+      if self.validations_disabled?
+        true
+      else
+        super
+      end
     end
+
+    def disable_validations!
+      @validations_disabled = true
+    end
+    
+    def enable_validations!
+      @validations_disabled = false
+    end
+
+    def validations_disabled?
+      @validations_disabled ||= false
+    end
+
+    def suppress_validations(&block)
+      disable_validations!
+      ret = self.instance_eval(&block) if block_given?
+      enable_validations!
+      ret
+    end
+    alias :without_validations :suppress_validations
+
+    protected
+      def run_validations!
+        self.validations_disabled? : true : super
+      end
+
   end
+  
 end
